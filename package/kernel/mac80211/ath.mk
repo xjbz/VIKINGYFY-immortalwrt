5f0af4b92745852a85d4b15e2d0135d61c7a6dca
VIKINGYFY <viking.yfy@qq.com>
2026-08-16 15:06:56 +0800

    stage 1

--- package/kernel/mac80211/ath.mk
+++ package/kernel/mac80211/ath.mk
@@ -3,54 +3,65 @@ PKG_DRIVERS += \
 	ath11k ath11k-ahb ath11k-pci ath12k carl9170 owl-loader ar5523 wil6210 qcom-qmi-helpers
 
 PKG_CONFIG_DEPENDS += \
-	CONFIG_ATH_USER_REGD \
+	CONFIG_PACKAGE_ATH_DEBUG \
+	CONFIG_PACKAGE_ATH_DFS \
+	CONFIG_PACKAGE_ATH_SPECTRAL \
+	CONFIG_PACKAGE_ATH_DYNACK \
 	CONFIG_ATH9K_HWRNG \
 	CONFIG_ATH9K_SUPPORT_PCOEM \
 	CONFIG_ATH9K_TX99 \
 	CONFIG_ATH10K_LEDS \
 	CONFIG_ATH10K_THERMAL \
-	CONFIG_ATH11K_DEBUGFS_HTT_STATS \
-	CONFIG_ATH11K_DEBUGFS_STA \
-	CONFIG_ATH11K_NSS_SUPPORT \
 	CONFIG_ATH11K_THERMAL \
 	CONFIG_ATH12K_THERMAL \
-	CONFIG_PACKAGE_ATH_DEBUG \
-	CONFIG_PACKAGE_ATH_DFS \
-	CONFIG_PACKAGE_ATH_DYNACK \
-	CONFIG_PACKAGE_ATH_SPECTRAL
+	CONFIG_ATH_USER_REGD
 
 ifdef CONFIG_PACKAGE_MAC80211_DEBUGFS
   config-y += \
-	ATH5K_DEBUG \
-	ATH6KL_DEBUG \
 	ATH9K_DEBUGFS \
 	ATH9K_HTC_DEBUGFS \
 	ATH10K_DEBUGFS \
 	ATH11K_DEBUGFS \
 	ATH12K_DEBUGFS \
 	CARL9170_DEBUGFS \
+	ATH5K_DEBUG \
+	ATH6KL_DEBUG \
 	WIL6210_DEBUGFS
 endif
 
 ifdef CONFIG_PACKAGE_MAC80211_TRACING
   config-y += \
-	ATH_TRACEPOINTS \
-	ATH5K_TRACER \
-	ATH6KL_TRACING \
 	ATH10K_TRACING \
 	ATH11K_TRACING \
 	ATH12K_TRACING \
+	ATH6KL_TRACING \
+	ATH_TRACEPOINTS \
+	ATH5K_TRACER \
 	WIL6210_TRACING
 endif
 
-config-$(call config_package,ar5523) += AR5523
+config-$(call config_package,qcom-qmi-helpers) += QCOM_QMI_HELPERS
 config-$(call config_package,ath,regular sdio smallbuffers) += ATH_CARDS ATH_COMMON
-config-$(call config_package,ath5k) += ATH5K ATH5K_PCI
-config-$(call config_package,ath6kl) += ATH6KL
-config-$(call config_package,ath6kl-sdio) += ATH6KL_SDIO
-config-$(call config_package,ath6kl-usb) += ATH6KL_USB
+config-$(CONFIG_PACKAGE_ATH_DEBUG) += ATH_DEBUG ATH10K_DEBUG ATH11K_DEBUG ATH12K_DEBUG ATH9K_STATION_STATISTICS
+config-$(CONFIG_PACKAGE_ATH_DFS) += ATH9K_DFS_CERTIFIED ATH10K_DFS_CERTIFIED
+config-$(CONFIG_PACKAGE_ATH_SPECTRAL) += ATH9K_COMMON_SPECTRAL ATH10K_SPECTRAL ATH11K_SPECTRAL
+config-$(CONFIG_PACKAGE_ATH_DYNACK) += ATH9K_DYNACK
 config-$(call config_package,ath9k) += ATH9K
 config-$(call config_package,ath9k-common) += ATH9K_COMMON
+config-$(call config_package,owl-loader) += ATH9K_PCI_NO_EEPROM
+config-$(CONFIG_TARGET_ath79) += ATH9K_AHB
+config-$(CONFIG_TARGET_ipq40xx) += ATH10K_AHB
+config-$(CONFIG_PCI) += ATH9K_PCI
+config-$(CONFIG_ATH_USER_REGD) += ATH_USER_REGD ATH_REG_DYNAMIC_USER_REG_HINTS
+config-$(CONFIG_ATH9K_HWRNG) += ATH9K_HWRNG
+config-$(CONFIG_ATH9K_SUPPORT_PCOEM) += ATH9K_PCOEM
+config-$(CONFIG_ATH9K_TX99) += ATH9K_TX99
+config-$(CONFIG_ATH9K_UBNTHSR) += ATH9K_UBNTHSR
+config-$(CONFIG_ATH10K_LEDS) += ATH10K_LEDS
+config-$(CONFIG_ATH10K_THERMAL) += ATH10K_THERMAL
+config-$(CONFIG_ATH11K_THERMAL) += ATH11K_THERMAL
+config-$(CONFIG_ATH12K_THERMAL) += ATH12K_THERMAL
+
 config-$(call config_package,ath9k-htc) += ATH9K_HTC
 config-$(call config_package,ath10k,regular) += ATH10K ATH10K_PCI
 config-$(call config_package,ath10k-sdio,sdio) += ATH10K ATH10K_SDIO
@@ -59,30 +70,17 @@ config-$(call config_package,ath11k) += ATH11K
 config-$(call config_package,ath11k-ahb) += ATH11K_AHB
 config-$(call config_package,ath11k-pci) += ATH11K_PCI
 config-$(call config_package,ath12k) += ATH12K
-config-$(call config_package,carl9170) += CARL9170
-config-$(call config_package,owl-loader) += ATH9K_PCI_NO_EEPROM
-config-$(call config_package,qcom-qmi-helpers) += QCOM_QMI_HELPERS
-config-$(call config_package,wil6210) += WIL6210
 
-config-$(CONFIG_ATH_USER_REGD) += ATH_USER_REGD ATH_REG_DYNAMIC_USER_REG_HINTS
-config-$(CONFIG_ATH9K_HWRNG) += ATH9K_HWRNG
-config-$(CONFIG_ATH9K_SUPPORT_PCOEM) += ATH9K_PCOEM
-config-$(CONFIG_ATH9K_TX99) += ATH9K_TX99
-config-$(CONFIG_ATH9K_UBNTHSR) += ATH9K_UBNTHSR
-config-$(CONFIG_ATH10K_LEDS) += ATH10K_LEDS
-config-$(CONFIG_ATH10K_THERMAL) += ATH10K_THERMAL
-config-$(CONFIG_ATH11K_DEBUGFS_HTT_STATS) += ATH11K_DEBUGFS_HTT_STATS
-config-$(CONFIG_ATH11K_DEBUGFS_STA) += ATH11K_DEBUGFS_STA
-config-$(CONFIG_ATH11K_NSS_SUPPORT) += ATH11K_NSS_SUPPORT ATH11K_NSS_MESH_SUPPORT ATH11K_MEM_PROFILE_512M
-config-$(CONFIG_ATH11K_THERMAL) += ATH11K_THERMAL
-config-$(CONFIG_ATH12K_THERMAL) += ATH12K_THERMAL
-config-$(CONFIG_PACKAGE_ATH_DEBUG) += ATH_DEBUG ATH10K_DEBUG ATH11K_DEBUG ATH12K_DEBUG ATH9K_STATION_STATISTICS
-config-$(CONFIG_PACKAGE_ATH_DFS) += ATH9K_DFS_CERTIFIED ATH10K_DFS_CERTIFIED
-config-$(CONFIG_PACKAGE_ATH_DYNACK) += ATH9K_DYNACK
-config-$(CONFIG_PACKAGE_ATH_SPECTRAL) += ATH9K_COMMON_SPECTRAL ATH10K_SPECTRAL ATH11K_SPECTRAL
-config-$(CONFIG_PCI) += ATH9K_PCI
-config-$(CONFIG_TARGET_ath79) += ATH9K_AHB
-config-$(CONFIG_TARGET_ipq40xx) += ATH10K_AHB
+config-$(call config_package,ath5k) += ATH5K ATH5K_PCI
+
+config-$(call config_package,ath6kl) += ATH6KL
+config-$(call config_package,ath6kl-sdio) += ATH6KL_SDIO
+config-$(call config_package,ath6kl-usb) += ATH6KL_USB
+
+config-$(call config_package,carl9170) += CARL9170
+config-$(call config_package,ar5523) += AR5523
+
+config-$(call config_package,wil6210) += WIL6210
 
 define KernelPackage/ath/config
   if PACKAGE_kmod-ath
@@ -341,13 +339,8 @@ define KernelPackage/ath11k
   URL:=https://wireless.wiki.kernel.org/en/users/drivers/ath11k
   DEPENDS+= +kmod-ath +@DRIVER_11AC_SUPPORT +@DRIVER_11AX_SUPPORT \
   +kmod-crypto-michael-mic +ATH11K_THERMAL:kmod-hwmon-core \
-  +ATH11K_THERMAL:kmod-thermal +kmod-qcom-qmi-helpers \
-  +ATH11K_NSS_SUPPORT:kmod-qca-nss-drv-wifi-meshmgr
+  +ATH11K_THERMAL:kmod-thermal +kmod-qcom-qmi-helpers
   FILES:=$(PKG_BUILD_DIR)/drivers/net/wireless/ath/ath11k/ath11k.ko
-ifdef CONFIG_ATH11K_NSS_SUPPORT
-  AUTOLOAD:=$(call AutoProbe,ath11k)
-  MODPARAMS.ath11k:=nss_offload=1 frame_mode=2
-endif
 endef
 
 define KernelPackage/ath11k/description
@@ -359,32 +352,8 @@ define KernelPackage/ath11k/config
 
        config ATH11K_THERMAL
                bool "Enable thermal sensors and throttling support"
-               depends on TARGET_qualcommax
                depends on PACKAGE_kmod-ath11k
-               default y
-
-       config ATH11K_NSS_SUPPORT
-               bool "Enable NSS WiFi Mesh offload"
-               depends on TARGET_qualcommax
-               default y
-               help
-                  Say Y to enable NSS WiFi offload support
-
-      config ATH11K_DEBUGFS_STA
-               bool "Enable ath11k station statistics"
-               depends on PACKAGE_kmod-ath11k
-               depends on PACKAGE_MAC80211_DEBUGFS
-               default y
-               help
-                  Say Y to enable access to the station statistics via debugfs.
-
-      config ATH11K_DEBUGFS_HTT_STATS
-               bool "Enable ath11k HTT statistics"
-               depends on PACKAGE_kmod-ath11k
-               depends on PACKAGE_MAC80211_DEBUGFS
-               default y
-               help
-                  Say Y to enable access to the HTT statistics via debugfs.
+               default y if TARGET_qualcommax
 
 endef
 
